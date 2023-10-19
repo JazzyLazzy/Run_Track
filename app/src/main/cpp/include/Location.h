6d0c92a769e85a1d939d8010a36c911b84adaab0
Lazarus Turpaud <pibob314@gmail.com>
2023-10-19 02:55:49 -0700

    Setup NDK, migrate to SDK 34, and setup firebase

---
+++ app/src/main/cpp/include/Location.h
@@ -0,0 +1,13 @@
+//Location_h
+#ifndef Location_H
+#define Location_H
+
+typedef struct {
+  double latitude;
+  double longitude;
+  int elevation;
+  unsigned long long time;
+  struct Location *next;
+} Location;
+
+#endif
---
+++ app/src/main/cpp/include/Track.h
@@ -0,0 +1,13 @@
+#include "Track_Seg.h"
+
+//Track_H
+#ifndef Track_H
+#define Track_H
+
+typedef struct{
+  char *name;
+  Track_Seg *track_segs;
+  struct Track *next;
+} Track;
+
+#endif
